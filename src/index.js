bc0c6fde5f65e9f234bfa7014b05898c6890a1d5
Trevor Bachofner <trevorb718208@gmail.com>
2025-06-25 10:58:18 -0700

    Fix homepage and routing for github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';  // make sure this path is correct
 import { store } from './app/store';
 import './index.css';
@@ -10,8 +10,8 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <Router>
         <App />
-        </BrowserRouter>
+        </Router>
     </Provider>
 );
